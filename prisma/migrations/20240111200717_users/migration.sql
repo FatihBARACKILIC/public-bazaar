7a79726c71d8eb6658384d56178fb20b25cd278f
FatihBARACKILIC <fatihb469@gmail.com>
2024-01-11 20:10:15 +0000

    Add user table and migration files

---
+++ prisma/migrations/20240111200717_users/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'SELLER');
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" UUID NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "username" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_username_key" ON "Users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
