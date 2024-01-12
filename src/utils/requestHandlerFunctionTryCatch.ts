37c4f38605c4c142ad73246b7cdaef3b34364ead
FatihBARACKILIC <fatihb469@gmail.com>
2024-01-12 17:37:29 +0000

    Add requestHandlerFunctionTryCatch utility function

---
+++ src/utils/requestHandlerFunctionTryCatch.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import RequestHandlerFunction from "../shared/types/requestHandlerFunction.type";
+
+const requestHandlerFunctionTryCatch = (
+  fn: RequestHandlerFunction<unknown>
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+};
+
+export default requestHandlerFunctionTryCatch;
