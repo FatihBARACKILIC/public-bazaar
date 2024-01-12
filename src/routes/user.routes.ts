8d03cab4c809a76d11d66d082f76716bc1598872
FatihBARACKILIC <fatihb469@gmail.com>
2024-01-12 20:11:54 +0000

    Add UserRoutes class for handling user-related routes

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,32 @@
+import type { Express } from "express";
+import BaseController from "../controllers/base.controllers";
+import UserController, {
+  IUserController,
+} from "../controllers/user.controllers";
+import requestHandlerFunctionTryCatch from "../shared/utils/requestHandlerFunctionTryCatch";
+import BaseRoutes from "./base.routes";
+
+class UserRoutes extends BaseRoutes {
+  protected readonly router: Express;
+  protected readonly routeMainURL: string;
+  protected readonly controller: BaseController & IUserController;
+
+  constructor(router: Express, routeMainURL: string = "/") {
+    super();
+
+    this.router = router;
+    this.routeMainURL = routeMainURL;
+    this.controller = new UserController();
+
+    this.runRoutes();
+  }
+
+  protected runRoutes(): void {
+    this.router.post(
+      `${this.routeMainURL}user`,
+      requestHandlerFunctionTryCatch(this.controller.register)
+    );
+  }
+}
+
+export default UserRoutes;
