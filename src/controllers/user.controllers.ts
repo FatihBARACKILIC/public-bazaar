a18c521271662c0439f77f3126375505c401ce5d
FatihBARACKILIC <fatihb469@gmail.com>
2024-01-12 20:11:59 +0000

    Add UserController with register method

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import UserServices, { IUserServices } from "../services/user.services";
+import { RegisterType } from "../shared/types/user.type";
+import BaseController from "./base.controllers";
+import BaseServices from "../services/base.services";
+
+interface IUserController {
+  register(req: Request, res: Response, next: NextFunction): Promise<void>;
+}
+
+class UserController extends BaseController implements IUserController {
+  protected readonly services: BaseServices & IUserServices;
+
+  constructor() {
+    super();
+
+    this.services = new UserServices();
+  }
+
+  register = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const newUser: RegisterType = req.body;
+    const response = await this.services.register(newUser);
+    res.status(200).json({
+      message: "User registered",
+      user: response,
+    });
+  };
+}
+
+export default UserController;
+export { IUserController };
